1b57bebc20effa7e63810f41c03695f5609e8d42
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-13 10:48:04 +0700

    membuat structuk object

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -1,2 +1,90 @@
 #include <iostream>
 using namespace std;
+
+// membuat struktur untuk object double linkedlist
+struct Node
+{
+    // deklarasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
+    // deklarasi pointer next dan pref untuk penunjukkan data sebelum dan sesudah
+    Node *next;
+    Node *prev;
+};
+
+// deklarasi pointer START dan pemberian nilai
+Node *START = NULL;
+
+// deklarasi prosedur addNode
+void addNode()
+{
+    // pembuatan node dan pemberian value untuk data noMhs dan name
+    Node *newNode = new Node(); // step 1 Buat node baru
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs; // assign value to theh data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name; // assign value to theh data field of the new node
+
+    // Insert the new node in the list 
+    //kondisi jika star == null atau noMhs node baru <= noMhs start
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        //step 2: insert new node at the beginning
+        // kondisi jika start tidak kosong dan noMhs node baru sma dengan noMhs
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+        //if the list is empaty, make the new node the START
+        //jika list kosong, maka node text nya adalah START
+        newNode->next = START; // step3; make the new node point to the fisrt node
+        // kondisi jika START tidak memiliki nilai atau tidak kosong 
+        if (START != NULL)
+        {
+            START->prev = newNode; // step 4; make the first node point to the new node 
+        }
+        //memberi nilai prev = null dan start = node baru 
+        newNode ->prev = NULL; // step 5: make the new node point to NULL
+        START = newNode; // step 6: make the new node the first node 
+    }
+    //kondisi jika semua kondisi if tidak terpenuhi
+    else
+    {
+
+        //insert the new node in the middle or at the end
+        //set nilai current = start dan nilai previous = null
+        Node *current = START; // step1.a: start from the first node
+        Node *previous = NULL; //step 1.b: previous node is Null intially
+
+        //Looping selama current != null dan noMhs dari current lebih kecil dari noMhs node baru
+        while (current !=NULL && current->noMhs < newNode->noMhs)
+        {                           //step 1.c: traverse the list to find the correct position
+            previous = current;     // step 1.d: move the previous to the current node
+            current = current->next; //step1.e: move the current to the next  node 
+        }
+
+        // set nilai next node baru = current dan prev node baru = previous 
+        newNode->next = current;  //step 4: make the next field of the new node 
+        newNode ->prev = previous; // step 5: make the previos field of teh new node
+
+        //kondisi jika current tidak sama dengan null
+        if (current != NULL)
+        {
+            current->prev = newNode; //step 6: make the previoud field of the current node point to the new node    
+        }
+        
+        //kondisijika previous tidak sama dengan null
+        if (previous != NULL)
+        {
+            previous->next = newNode; //step 7: make the next field of the previos node point to the new node 
+        }
+        // kondisi jika if previous sama dengan NULL
+        else
+        {
+            // if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
+    }
+}
+
